733da63eecd3f6710dbdb48f6695f17fa714c681
ken <testlee@126.com>
2015-06-10 14:32:57 +0800

    add lookup event for camera list looking up

--- app/socketHandler.js
+++ app/socketHandler.js
@@ -1,36 +1,41 @@
-module.exports = function(io, streams) {
+module.exports = function (io, streams) {
 
-  io.on('connection', function(client) {
-    console.log('-- ' + client.id + ' joined --');
-    client.emit('id', client.id);
+    io.on('connection', function (client) {
+        console.log('-- ' + client.id + ' joined --');
+        client.emit('id', client.id);
 
-    client.on('message', function (details) {
-      var otherClient = io.sockets.connected[details.to];
+        client.on('message', function (details) {
+            var otherClient = io.sockets.connected[details.to];
 
-      if (!otherClient) {
-        return;
-      }
-        delete details.to;
-        details.from = client.id;
-        otherClient.emit('message', details);
+            if (!otherClient) {
+                return;
+            }
+            delete details.to;
+            details.from = client.id;
+            otherClient.emit('message', details);
+        });
+
+        client.on('lookup', function () {
+            client.emit('cameraList', streams);
+        });
+
+
+        client.on('readyToStream', function (options) {
+            console.log('-- ' + client.id + ' is ready to stream --');
+
+            streams.addStream(client.id, options.name);
+        });
+
+        client.on('update', function (options) {
+            streams.update(client.id, options.name);
+        });
+
+        function leave() {
+            console.log('-- ' + client.id + ' left --');
+            streams.removeStream(client.id);
+        }
+
+        client.on('disconnect', leave);
+        client.on('leave', leave);
     });
-      
-    client.on('readyToStream', function(options) {
-      console.log('-- ' + client.id + ' is ready to stream --');
-      
-      streams.addStream(client.id, options.name); 
-    });
-    
-    client.on('update', function(options) {
-      streams.update(client.id, options.name);
-    });
-
-    function leave() {
-      console.log('-- ' + client.id + ' left --');
-      streams.removeStream(client.id);
-    }
-
-    client.on('disconnect', leave);
-    client.on('leave', leave);
-  });
 };
